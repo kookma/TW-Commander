7afaff8fb2843b644582bc0173cff7724a850590
Mark S <throaway@yahoo.com>
2019-03-14 10:14:39 -0700

    190314 Upload project as tiddlers.

---
+++ tiddlers/$__commander_macro_snr_regexpsub.js
@@ -0,0 +1,47 @@
+/*\
+title: $:/commander/macro/snr/regexpsub.js
+type: application/javascript
+module-type: macro
+
+Make regular expression substitutions
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports.name = "regexpsub";
+
+exports.params = [
+	{name: "searchValue"},
+	{name: "replaceValue"},
+	{name: "sourceText"},
+	{name: "flags"},
+    {name: "wholeWords"}
+];
+
+/*
+Run the macro
+*/
+exports.run = function(searchValue, replaceValue, sourceText, flags = "gi", wholeWords = "characters") {
+  
+  try {
+  
+	var searchText;
+    if(wholeWords.toLowerCase() === 'words'){
+    searchText = "\\b" + searchValue + "\\b";
+    } else{
+    searchText = searchValue;
+    }
+    searchText = new RegExp(searchText, flags);
+
+	
+	return sourceText.replace(searchText,replaceValue);
+
+  } catch(err) { return "ERROR IN REG EXPRESSION. YOU MAY NEED TO ESCAPE VALUES"; }
+  
+  };
+
+})();
